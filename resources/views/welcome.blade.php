60b9c5d6a874c35ee5330aa7a3ba00cedd984802
zidna110 <1439276668@qq.com>
2017-12-28 04:48:33 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 66px;
+            }
+        </style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Hello Laravel! -by chen14</div>
+        </div>
+    </div>
+</body>
+</html>
